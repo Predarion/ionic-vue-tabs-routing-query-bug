a567358f08bf2de02db6dad588111a3227dd4ccf
Manuel Göster <manuel.goester@ambalytics.com>
2021-11-12 10:44:33 +0100

    Adds reproduction

--- src/views/Tab2.vue
+++ src/views/Tab2.vue
@@ -11,18 +11,42 @@
           <ion-title size="large">Tab 2</ion-title>
         </ion-toolbar>
       </ion-header>
-      
+
       <ExploreContainer name="Tab 2 page" />
+      <RouterLink
+        :to="{
+          name: 'Tab2Subpage',
+          query: {
+            test: 'this-is-a-test-value',
+          },
+        }"
+        >Go to: /tabs/tab2/subpage</RouterLink
+      >
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+} from "@ionic/vue"
+import ExploreContainer from "@/components/ExploreContainer.vue"
+import { RouterLink } from "vue-router"
 
-export default  {
-  name: 'Tab2',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+export default {
+  name: "Tab2",
+  components: {
+    ExploreContainer,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonPage,
+    RouterLink,
+  },
 }
 </script>
